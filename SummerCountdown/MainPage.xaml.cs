eb8843cfc5e3d0bb55645db764ca0ae36237c9e1
АМИР <@ABISHEV-AMIR>
2026-09-17 23:33:58 +0500

    Добавьте файлы проекта.

---
+++ SummerCountdown/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace SummerCountdown;
+
+public partial class MainPage : ContentPage
+{
+    private IDispatcherTimer _timer = null!;
+    private DateTime _targetDate = new DateTime(2027, 6, 1, 0, 0, 0); // 1 июня 2027 года
+
+    public MainPage()
+    {
+        InitializeComponent();
+        SetupTimer();
+    }
+
+    private void SetupTimer()
+    {
+        _timer = Dispatcher.CreateTimer();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += (s, e) => UpdateCountdown();
+        _timer.Start();
+    }
+
+    private void UpdateCountdown()
+    {
+        var now = DateTime.Now;
+        if (now >= _targetDate) return;
+
+        var timeLeft = _targetDate - now;
+
+        int months = (_targetDate.Year - now.Year) * 12 + _targetDate.Month - now.Month;
+        if (_targetDate.Day < now.Day) months--;
+
+        int weeks = (int)(timeLeft.Days / 7);
+        int days = timeLeft.Days % 7;
+        int hours = timeLeft.Hours;
+        int minutes = timeLeft.Minutes;
+        int seconds = timeLeft.Seconds;
+
+        MonthsLabel.Text = months.ToString();
+        WeeksLabel.Text = weeks.ToString();
+        DaysLabel.Text = days.ToString();
+        HoursLabel.Text = hours.ToString();
+        MinutesLabel.Text = minutes.ToString();
+        SecondsLabel.Text = seconds.ToString();
+    }
+}
